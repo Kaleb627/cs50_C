a6ff8f765de0f14e679ba8b79cba703706d59942
Kaleb627 <Kaleb6277@gmail.com>
2022-07-17 14:18:07 -0400

    Update caeser.c
    comments

--- caeser.c
+++ caeser.c
@@ -6,12 +6,14 @@
 
 int main(int argc, string argv[])
 {
+    //checking that only 2 arguments were input in command line
     if (argc != 2)
     {
         printf("Usage: ./caeser key \n");
         return 1;
     }
 
+    //checking that the second argument is only digits
     for (int i = 0; i < strlen(argv[1]); i++)
     {
         if (!isdigit(argv[1][i]))
@@ -26,6 +28,7 @@ int main(int argc, string argv[])
     string plainText = get_string("Enter text to be cyphered:  ");
     printf("Cyphertext:  ");
 
+    //creating cypher taking capitalizaion into account and ignoring non alpha characters
     for (int j = 0; j < strlen(plainText); j++)
     {
         if (isupper(plainText[j]))
